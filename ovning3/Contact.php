d7ea18bbfe94d5782d1a8c126be04a85d0c42d14
TobiasWeste <vbnmtobbe@gmail.com>
2017-04-06 10:46:03 +0300

    Add files via upload
    Övning 3

---
+++ ovning3/Contact.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Övning 3</title>
+    <link rel="stylesheet" href="CSS/bootstrap.min.css">
+    <link href="CSS/ovning3.css" rel="stylesheet">
+  </head>
+  <body>
+
+<!-- HERO-contents -->
+  <header>
+    <div>
+      <h1>Företag Ab</h1>
+    </div>
+  </header>
+
+  <!-- NAVIGATION -->
+  <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">Navbar</a>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Pictures.php">Photos</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="Contact.php">Contact<span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+  </div>
+  </nav>
+
+
+  <div>
+    <h1>Kontakt...</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>
+        <br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+  </div>
+  <!-- Contact form -->
+  <form>
+    <input type="text" placeholder="First name" required>
+    <input type="text" placeholder="Last name">
+    <input type="email" placeholder="E-mail" required>
+    <textarea placeholder="Your message..."></textarea>
+    <input type="submit" value="Send message">
+  </form>
+
+  <!-- FOOTER -->
+  <footer>&copy;<?php echo date("Y"); ?></footer>
+
+  <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+  <script src="js/bootstrap.min.js"></script>
+
+  </body>
+</html>
